3d4ab493e4c3266586d6c09fb15f8f4711f208f1
Sujanth <sujanth1987@gmail.com>
2017-08-20 19:35:43 +1000

    Initial Commit

---
+++ TemperatureCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TemperatureCalculator
+//
+//  Created by Sujanth Sebamalaithasan on 20/8/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
