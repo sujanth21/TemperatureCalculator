0f628badd5f9cdb359ee8eb588320601ce1e7472
Sujanth <sujanth1987@gmail.com>
2017-08-20 19:48:40 +1000

    Actions added

--- TemperatureCalculator/ViewController.swift
+++ TemperatureCalculator/ViewController.swift
@@ -27,6 +27,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func temperatureEntered(_ sender: Any) {
+    }
+    
+    @IBAction func temperatureToggled(_ sender: Any) {
+    }
+    
+    
 }
 
